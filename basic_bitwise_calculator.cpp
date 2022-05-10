3b035efb7f84dec4c53ac4b1ed22d4808bad9d75
Liam McArdle <lmcardle@uwaterloo.ca>
2022-05-10 15:54:00 -0400

    Bug fixes and formatting improvements

--- basic_bitwise_calculator.cpp
+++ basic_bitwise_calculator.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <algorithm> 
 #include <string>
+#include <chrono>
+#include <thread>
 
 int main();
 unsigned int AND(unsigned int val1, unsigned int val2);
@@ -16,9 +18,15 @@ int main() {
 	unsigned int result = 0;
 
 	while (stop == 0) {
+		using namespace std::this_thread; // sleep_for, sleep_unt
+		using namespace std::chrono; // nanoseconds, system_clock, seconds
+						  
+		sleep_for(nanoseconds(10));
+		sleep_until(system_clock::now() + seconds(1));
+
 		unsigned int val1 = 0;
 		unsigned int val2 = 0; 
-		std::cout << "Welcome to Bitwise Calculator!\n1.Bitwise AND\n2.Bitwise OR\n3.Bitwise XOR\n4.Display previous result in binary\n5.Display previous result in Hexadecimal\n6.Exit"          		  << std::endl;
+		std::cout << "Welcome to Bitwise Calculator!\n1.Bitwise AND\n2.Bitwise OR\n3.Bitwise XOR\n4.Display previous result in binary\n5.Display previous result in hexadecimal\n6.Exit"          		  << std::endl;
 		std::cout << "Please input a value : ";
 		std::cin >> value; 
 
@@ -29,6 +37,7 @@ int main() {
 			std::cout << "Please input the second value (non-negative) : ";
 			std::cin >> val2;
 			std::cout << std::endl;
+			std::cout << "The result is : ";
 			result = AND(val1, val2);
 		} else if (value == 2) {
 			std::cout << "Please input the first value (non-negative) : ";
@@ -37,6 +46,7 @@ int main() {
 			std::cout << "Please input the second value (non-negative) : ";
 			std::cin >> val2;
 			std::cout << std::endl;
+			std::cout << "The result is : ";
 			result = OR(val1, val2);
 		} else if (value == 3) {
 			std::cout << "Please input the first value (non-negative) : ";
@@ -45,17 +55,21 @@ int main() {
                         std::cout << "Please input the second value (non-negative): ";
 			std::cin >> val2;
 			std::cout << std::endl;
+			std::cout << "\nThe result is : ";
 			result = XOR(val1, val2); 
 		} else if (value == 4) {
-			std::cout << "The result in binary is : ";
-			result_to_bin(result);
+			std::cout << "The result in binary is : 0b";
+			if(result == 1) {
+				std::cout << 1 << "\n";
+			}
+		       	result_to_bin(result);
 		}else if (value == 5) {
 			result_to_hex(result);
 		} else if (value == 6) {
-			std::cout << "Thank you, have a nice day." << std::endl;
+			std::cout << "\nThank you, have a nice day.\n" << std::endl;
 			stop++;
 		} else {
-			std::cout << "Invalid value entered, please try again." << std::endl;
+			std::cout << "\nInvalid value entered, please try again." << std::endl;
 		}																			  
 	}	
 }
@@ -63,18 +77,21 @@ int main() {
 unsigned int AND(unsigned int val1, unsigned int val2) {
 	unsigned int result = (val1 & val2);
 	std::cout << result << std::endl;
+	std::cout << std::endl;
 	return result;
 }
 
-unsigned int OR(int val1, int val2) {
+unsigned int OR(unsigned int val1, unsigned int val2) {
 	unsigned int result = (val1 | val2);
 	std::cout << result << std::endl;
+	std::cout <<std::endl;
 	return result;
 }
 
 unsigned int XOR(unsigned int val1, unsigned int val2) {
 	unsigned int result = (val1 ^ val2);
 	std::cout << result << std::endl;
+	std::cout << std::endl;
 	return result;
 }
 
@@ -106,5 +123,5 @@ void result_to_hex(unsigned int result) {
 		product *= 10;
 						    }
 		std::reverse(hex_dec.begin(), hex_dec.end());
-		std::cout << "The result in the hexadecimal form is: " << hex_dec << std::endl;
+		std::cout << "\nThe result in the hexadecimal form is: 0x" << hex_dec << "\n" << std::endl;
 }
